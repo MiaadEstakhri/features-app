fb6ac2a22ddb2ee0a0baccf1bae96cefc7ccfe62
MiaadEstakhri <miaad8104@gmail.com>
2023-09-09 22:25:40 +0330

    change import

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
-import "./index.css";
+import "./assets/styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>

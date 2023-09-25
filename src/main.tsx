4d52d11645e43d4f825669402d426359bd69e505
MiaadEstakhri <miaad8104@gmail.com>
2023-09-25 21:43:14 +0330

    add pwa

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import "./assets/styles/index.css";
+import { registerSW } from "virtual:pwa-register";
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("New content available. Reload?")) {
+      updateSW(true);
+    }
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
